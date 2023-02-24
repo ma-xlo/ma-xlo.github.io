d5e0c052a71a371b68f7eafd29902bcd6675bd18
ma-xlo <macorrea-@hotmail.com>
2023-02-23 23:36:04 -0300

    fix: adjust projects gallery size

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,16 @@
-const text = document.querySelector("#text");
+const sendButton = document.querySelector('#send-btn');
+const senderName = document.querySelector('#sender-name');
+const emailSubject = document.querySelector('#email-subject');
+const emailContent = document.querySelector('#email-content');
+const text = document.querySelector('#text');
 const raised = document.querySelector("#raised-hand");
 const weaving = document.querySelector("#weaving-hand");
+
+
+
+sendButton.addEventListener('click', () => {
+    document.location = `mailto:macorrea-@hotmail.com?subject=${emailSubject.textContent}&body=${emailContent.value + senderName.textContent}`
+})
 
 setInterval(() => {
     raised.classList.toggle("hide");
