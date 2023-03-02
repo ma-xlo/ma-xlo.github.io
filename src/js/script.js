47325918009c8255e7a8acb49ab9d9b055392686
ma-xlo <macorrea-@hotmail.com>
2023-03-02 00:19:44 -0300

    feat: add scroll fade in over the sections and fix mailto output

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,36 @@
 const sendButton = document.querySelector('#send-btn');
 const senderName = document.querySelector('#sender-name');
 const emailSubject = document.querySelector('#email-subject');
-const emailContent = document.querySelector('#email-content');
+let emailContent = document.querySelector('#email-content');
 const text = document.querySelector('#text');
-const raised = document.querySelector("#raised-hand");
-const weaving = document.querySelector("#weaving-hand");
-
+const blockCursor = document.querySelector("#block-cursor");
 
 
 sendButton.addEventListener('click', () => {
-    document.location = `mailto:macorrea-@hotmail.com?subject=${emailSubject.textContent}&body=${emailContent.value + senderName.textContent}`
+    document.location = `mailto:macorrea-@hotmail.com?subject=${emailSubject.value}&body=${emailContent.value.replaceAll('\n', '%0D%0A')}%0D%0D%0A%0D%0ASincerely, ${senderName.value} `
 })
 
 setInterval(() => {
-    raised.classList.toggle("hide");
-    weaving.classList.toggle("hide");
-}, 400);
+    blockCursor.classList.toggle("hide");
+}, 600);
+
+window.addEventListener('scroll', reveal);
+
+function reveal (){
+    let reveals = document.querySelectorAll('.reveal');
+
+    for(let i = 0; i < reveals.length; i++) {
+        let windowHeight = window.innerHeight;
+        let revealTop = reveals[i].getBoundingClientRect().top;
+        let revealPoint = 250;
+
+        if(revealTop < windowHeight - revealPoint) {
+            reveals[i].classList.add('active')
+        }
+        else {
+            reveals[i].classList.remove('active');
+        }
+    }
+}
 
 
